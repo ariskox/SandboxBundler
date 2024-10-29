f4b545b138c65d41e43b8228d5f9abd9ba38da7d
Aris Koxaras <aris@koxaras.com>
2024-10-29 15:15:03 +0200

    Initial Commit

---
+++ SandboxBundler/SandboxBundlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxBundlerApp.swift
+//  SandboxBundler
+//
+//  Created by Aris Koxaras on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SandboxBundlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
