a377179f2800d8a372c567ecffa715913d825b51
Aris Koxaras <aris@koxaras.com>
2024-10-29 23:18:08 +0200

    Always choose dark theme. Terminate after last window is closed

---
+++ SandboxBundler/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SandboxBundler
+//
+//  Created by Aris Koxaras on 29/10/24.
+//
+
+import Foundation
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
