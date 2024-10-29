37b9de409a1cf5f66a45b5bdd04c69ef2bd1ff1b
Aris Koxaras <aris@koxaras.com>
2024-10-29 18:07:18 +0200

    Simplified Droppable View

--- SandboxBundler/Classes/Droppable.swift
+++ SandboxBundler/Classes/Droppable.swift
@@ -14,46 +14,44 @@ struct Droppable: View {
     @State private var isDropping = false
 
     var body: some View {
-        if binaryFile != nil {
-            Rectangle()
-                .fill(isInvalid ? Color.red.opacity(0.5) : Color.green.opacity(0.5))
-                .cornerRadius(20) // Rounded corners
-                .frame(width: 250, height: 80)
-                .overlay {
-                    Text(archTitle)
-                        .foregroundColor(.white)
-                        .font(.body)
-                        .multilineTextAlignment(.center)
-                }
-                .padding()
-                .onDrop(
-                    of: [.fileURL],
-                    delegate: FileDropDelegate(
-                        binaryFile: $binaryFile,
-                        isDropping: $isDropping
-                    )
+        Rectangle()
+            .fill(fillColor)
+            .cornerRadius(20) // Rounded corners
+            .border(isDropping ? Color.blue : Color.clear, width: 4)
+            .frame(width: 250, height: 80)
+            .overlay {
+                Text(overlayTitle)
+                    .foregroundColor(overlayTextColor)
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            .onDrop(
+                of: [.fileURL],
+                delegate: FileDropDelegate(
+                    binaryFile: $binaryFile,
+                    isDropping: $isDropping
                 )
+            )
+    }
 
+    var overlayTextColor: Color {
+        return (binaryFile != nil) ? .white : .black
+    }
+
+    var overlayTitle: String {
+        if binaryFile != nil {
+            return archTitle
         } else {
-            Rectangle()
-                .fill(Color.gray.opacity(0.5))
-                .cornerRadius(20) // Rounded corners
-                .border(isDropping ? Color.blue : Color.clear, width: 4)
-                .frame(width: 250, height: 80)
-                .overlay {
-                    Text(architecture.dragAndDropTitle)
-                        .foregroundColor(.black)
-                        .font(.headline)
-                        .multilineTextAlignment(.center)
-                }
-                .padding()
-                .onDrop(
-                    of: [.fileURL],
-                    delegate: FileDropDelegate(
-                        binaryFile: $binaryFile,
-                        isDropping: $isDropping
-                    )
-                )
+            return architecture.dragAndDropTitle
+        }
+    }
+
+    var fillColor: Color {
+        if binaryFile != nil {
+            return isInvalid ? Color.red.opacity(0.5) : Color.green.opacity(0.5)
+        } else {
+            return Color.gray.opacity(0.5)
         }
     }
 
